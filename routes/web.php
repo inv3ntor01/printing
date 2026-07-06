c206bc898a4fffc2d5d8cae0734e529ce2ab1d1b
inv3ntor01 <inv3ntor01@users.noreply.github.com>
2026-07-06 13:28:54 +0800

    task: added print route

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,9 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
 });
 
+Route::get('/print', function () {
+    return 'Document printing service coming soon!';
+});
+
 require __DIR__.'/settings.php';
 require __DIR__.'/admin.php';
